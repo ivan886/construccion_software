5bca4f011d925740e3d6f01c398c1b9a4ca6bc40
ivan <ivanfernando.fonseca@gmail.com>
2021-09-08 13:29:43 -0500

    first commit

---
+++ Album2/Jugador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Album2
+{   public enum Posicion
+    {   Delantero,
+        Lateral,
+        Volante,
+        Defensor,
+        Arquero
+    }
+    
+    public class Jugador
+    {   private string nombres;
+        private string apellidos;
+        private DateTime fechaNacimiento ;
+        private float peso;
+        private float altura;
+        private string imagen;
+        private Posicion posicion;
+
+        public Jugador(string nombres, DateTime fechaNacimiento)
+        {
+            this.nombres = nombres;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+      
+        public Jugador(string nombres, string apellidos, DateTime fechaNacimiento, float peso, float altura, string imagen, Posicion posicion)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.fechaNacimiento = fechaNacimiento;
+            this.peso = peso;
+            this.altura = altura;
+            this.imagen = imagen;
+            this.posicion = posicion;
+        }
+
+        public int getEdad()
+        {   int currentYear = DateTime.Now.Year;
+            int year = fechaNacimiento.Year;
+            return currentYear-year;
+        }
+
+    }
+}
---
+++ Album2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Album2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaNacimiento1 = new DateTime(2000,5, 7);
+            Jugador jugador1 = new Jugador("FRANCO",    fechaNacimiento1 );
+            Jugador jugador2 = new Jugador("JUAN MARCOS",fechaNacimiento1);
+
+            int edad = jugador1.getEdad();
+            Console.WriteLine(edad);
+
+        }
+    }
+}
